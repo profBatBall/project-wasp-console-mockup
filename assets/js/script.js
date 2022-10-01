492754eeeb9fbf87614de1f09347595730edfd44
profBatBall <vic.vickyy@gmail.com>
2022-10-01 22:45:49 +0530

    Navbar Added + LeftNav Bug Fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Nav Dark Mode Toggle
 const body = document.querySelector('body'),
   sidebar = body.querySelector('nav'),
   toggle = body.querySelector('.toggle'),
@@ -16,4 +17,33 @@ modeSwitch.addEventListener('click', () => {
   } else {
     modeText.innerText = 'Dark mode';
   }
+});
+
+// Navbar
+const navBar = document.querySelector('.navbar');
+allLi = document.querySelectorAll('.navbar ul li');
+
+allLi.forEach((li, index) => {
+  li.addEventListener('click', (e) => {
+    e.preventDefault(); //preventing from submitting
+    navBar.querySelector('.active').classList.remove('active');
+    li.classList.add('active');
+
+    const indicator = document.querySelector('.indicator');
+    indicator.style.transform = `translateX(calc(${index * 90}px))`;
+  });
+});
+
+// Text Effects
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((el) => {
+  el.addEventListener('blur', (e) => {
+    if (e.target.value) {
+      e.target.classList.add('dirty');
+    } else {
+      e.target.classList.remove('dirty');
+    }
+  });
 });
